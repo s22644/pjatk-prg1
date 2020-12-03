b62984266510e31ee6d3eb3be1d88b33d13ea6c4
s22644 <73029854+s22644@users.noreply.github.com>
2020-12-03 20:12:02 +0100

    Update s03-countdown.cpp

--- src/s03-countdown.cpp
+++ src/s03-countdown.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <string>
+#include <random>
 int main() {
-  auto a = int{};
-  auto x = int{};
+    
+    std::string x;
+    std::random_device rd;
+    std::uniform_int_distribution<int> d100 (1, 100);
 
-  for (auto x = a; x; --x) {
-    
-    std::cout << " " << a;
-    std::cout << " " << x << "\n";
-  }
-    std::cout << " " << x << "\n";
-    
+    auto const x = d100(rd);
+
+  std::cout << "Zgadnij liczbę w przedziale od 1 do 100: ";
+  std::getline (std::cin, x);
+
+    while (y != x) {
+
+    if (y < x) 
+        std::cout << "Za mało" '\n';
+        std::getline (std::cin, x);
+        
+    else if (y > x)
+        std::cout << "Za dużo"'\n';
+        std::getline (std::cin, x);
+    }
+    std::cout << "Zgadłeś, brawo!"'\n';
+
 return 0;
-}
+} 
