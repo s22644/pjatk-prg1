6d8ab646d745f17e4b81609122ec75269978781c
s22644 <73029854+s22644@users.noreply.github.com>
2020-12-03 20:22:06 +0100

    Update s03-fizzbuzz.cpp

--- src/s03-fizzbuzz.cpp
+++ src/s03-fizzbuzz.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-using namespace std;
-int main()
-{
-int n;
-    std::cout << "Sprawdź czy liczba jest podzielna przez 3 lub 5,\nWpisz wartość: ";
-    std::cin >> n;
-    if(n%15 == 0)
-        std::cout << n << " Fizzbuzz";
-    else if(n%3 == 0)
-        std::cout << n << " Fizz";    
-    else if(n%5 == 0)
-        std::cout << n << " Buzz";
-    else
-        std::cout << "Liczba nie jest podzielna przez 3 ani 5";
+
+int main() {
+  int limit;
+  std::cout
+      << "Sprawdź czy liczba jest podzielna przez 3 lub 5,\nWpisz wartość: ";
+  std::cin >> limit;
+    for (int i = 1; i <= limit; i++) {
+        if (i % 15 == 0) { 
+            std::cout << i << " FizzBuzz";
+        }else if (i % 3 == 0) {
+            std::cout << i << " Fizz";
+        }else if (i % 5 == 0) {
+            std::cout << i << " Buzz";
+        }else {
+            std::cout << "Liczba nie jest podzielna przez 3 ani 5";
+        }
+}      
 return 0;
 }
